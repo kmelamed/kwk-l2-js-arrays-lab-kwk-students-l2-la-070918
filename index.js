8e8921d694ec367b497f24b3ef58e95ee1789db0
kmelamed <kmelamed@buckley.org>
2018-07-13 23:38:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name) {
+  drivers.push(name);
+  return drivers;
+}
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift(name);
+  return drivers;
+}
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop();
+  return drivers;
+}
+
+function destructivelyRemoveFirstDriver() {
+  drivers.shift();
+  return drivers;
+}
+
+function appendDriver(name) {
+  var theName = [name]
+  return drivers.concat(theName);
+}
+
+function prependDriver(name) {
+  var newArr = drivers.slice(0);
+  newArr.unshift(name);
+  return newArr;
+}
+
+function removeLastDriver() {
+  var newArr = driver.slice(0);
+  newArr.pop();
+  return newArr;
+}
